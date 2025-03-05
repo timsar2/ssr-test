d2cb59b9cceccc476255aa5347d37d840d418df3
timsar2 <info.golab@gmail.com>
2025-03-05 21:01:50 +0330

    rename to -

--- src/app/app-config-server.ts
+++ src/app/app-config-server.ts
@@ -2,7 +2,7 @@ import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+import { serverRoutes } from './app-routes-server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
