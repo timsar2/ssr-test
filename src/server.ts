854a8de65e4fc4bebc2fe1bbd77fb9759c59913e
timsar2 <info.golab@gmail.com>
2025-03-06 09:39:32 +0330

    .

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,41 @@
-import { AngularAppEngine, createRequestHandler } from '@angular/ssr'
-import { getContext } from '@netlify/angular-runtime/context'
+import { CommonEngine } from "@angular/ssr/node";
+import { render } from "@netlify/angular-runtime/common-engine";
 
-const angularAppEngine = new AngularAppEngine()
+const commonEngine = new CommonEngine();
+
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  return await render(commonEngine);
+}
+
+// If you want to use "AppEngine" instead - your server.ts file should contain following:
+
+import { AngularAppEngine, createRequestHandler } from "@angular/ssr";
+import { getContext } from "@netlify/angular-runtime/context";
+
+const angularAppEngine = new AngularAppEngine();
 
 export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
-  const context = getContext()
+  const context = getContext();
 
-  const result = await angularAppEngine.handle(request, context)
-  return result || new Response('Not found', { status: 404 })
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  const result = await angularAppEngine.handle(request, context);
+  return result || new Response("Not found", { status: 404 });
 }
 
 /**
  * The request handler used by the Angular CLI (dev-server and during build).
  */
-export const reqHandler = createRequestHandler(netlifyAppEngineHandler)
+export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
